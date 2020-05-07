9b3b9d419cd368488d33da5a9cbeb4eb3829d985
nightbladesten <nightbladesten@users.noreply.github.com>
2020-05-07 22:58:09 +0200

    Implement clean_data & save_data Finish process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -2,17 +2,16 @@ import sys
 
 import pandas as pd
 from pandas import DataFrame
+from sqlalchemy import create_engine
 
 
-def load_data(messages_filepath, categories_filepath):
-    """Receives the paths to two csv files and transforms them into a Pandas DataFrames.
-
-    Parameters:
-        messages_filepath - (string) Path to the messages csv.
-        categories_filepath - (string) Path to the categories csv.
-
-    Returns:
-        df - Pandas DataFrame
+def load_data(messages_filepath: str, categories_filepath: str) -> DataFrame:
+    """Receives the paths to two csv files and transforms them into a common Pandas DataFrames.
+        :param messages_filepath: (string) Path to the messages csv.
+        :param categories_filepath: (string) Path to the categories csv.
+        
+        :return: df - Pandas DataFrame merged from the two input csv.
+        :rtype: Pandas DataFrame
     """
     df: DataFrame = pd.DataFrame()
     try:
@@ -35,11 +34,40 @@ def load_data(messages_filepath, categories_filepath):
 
 
 def clean_data(df):
-    pass
+    """Receives the Pandas DataFrame and cleans up the categories labels, sorting them into separate
+    columns.
+        :param: df: (DataFrame) Data coming from the load_data() method.
+
+        :return: clean_df: (DataFrame) Categories rearranged into columns and duplicate data removed.
+    """
+    clean_df: DataFrame = df.drop('categories', axis=1).copy()
+    try:
+        categories = df.categories.str.split(';', expand=True)
+        row = categories.iloc[0]
+        categories.columns = [cat.partition('-')[0] for cat in row]
+        for column in categories:
+            # set each value to be the last character of the string
+            categories[column] = categories[column].astype(str).str[-1]
+
+            # convert column from string to numeric
+            categories[column] = pd.to_numeric(categories[column])
+
+        clean_df = pd.concat([df, categories], axis=1).drop_duplicates()
+
+    except:
+        print('Unknown error while cleaning the data.')
+    
+    finally:
+        return clean_df
 
 
 def save_data(df, database_filename):
-    pass
+    """
+        :arg df: (DataFrame) Pandas DataFrame to be saved
+        :arg database_filename: (string) Name of the SQL Database
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
@@ -70,3 +98,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+ #+
