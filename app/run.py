478fde5b30fc7ba9c3912427f0a152ca34e951f9
stluc <iamjuststen@gmail.com>
2020-05-15 18:57:11 +0200

    [UPDATE] First implementation

--- app/run.py
+++ app/run.py
@@ -1,50 +1,68 @@
 import json
-import plotly
+import re
+import warnings
+
+import joblib
+import nltk
 import pandas as pd
-
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import word_tokenize
-
+import plotly
 from flask import Flask
 from flask import render_template, request, jsonify
+from nltk.corpus import stopwords, wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from plotly.graph_objs import Bar
-from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
+nltk.download(['punkt', 'wordnet', 'stopwords', 'words'], quiet=True)
+warnings.filterwarnings("ignore", category=UserWarning)
 
 app = Flask(__name__)
 
-def tokenize(text):
-    tokens = word_tokenize(text)
-    lemmatizer = WordNetLemmatizer()
 
+def tokenize(text):
+    """
+    :arg text: (string) text to process
+    :return clean_tokens: (list) of lemmatized tokens
+    """
+
+    # Normalize text
+    text = re.sub(r'[^a-zA-Z0-9]', " ", text)
+    # Tokenize text
+    tokens = word_tokenize(text)
+    # Remove stop words
+    tokens = [w for w in tokens if w not in stopwords.words('english')]
+
+    lemmatizer = WordNetLemmatizer()
     clean_tokens = []
     for tok in tokens:
-        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tok = lemmatizer.lemmatize(tok.lower().strip())
         clean_tokens.append(clean_tok)
 
     return clean_tokens
 
+
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('DisasterResponse', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/model.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
 @app.route('/')
 @app.route('/index')
 def index():
-    
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
-    genre_counts = df.groupby('genre').count()['message']
+    genre_counts = df.groupby('genre').count()['message']/df['message'].count()*100
     genre_names = list(genre_counts.index)
-    
+
+    cat_perc = df.drop(['message', 'original', 'genre'], axis=1).sum() / len(df) * 100
+    cat_perc = cat_perc.sort_values()
+    cat_names = cat_perc.index.to_list()
+
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
         {
             'data': [
@@ -57,19 +75,43 @@ def index():
             'layout': {
                 'title': 'Distribution of Message Genres',
                 'yaxis': {
-                    'title': "Count"
+                    'title': "Percentage [%]",
+                    'showgrid': True
                 },
                 'xaxis': {
                     'title': "Genre"
-                }
+                },
+                'hoverinfo': 'y'
+            }
+        },
+
+        {
+            'data': [
+                Bar(
+                    x=cat_perc,
+                    y=cat_names,
+                    orientation='h'
+                )
+            ],
+
+            'layout': {
+                'title': 'Distribution by Categories',
+                'yaxis': {
+                    'title': "Category"
+                },
+                'xaxis': {
+                    'title': "Percentage [%]",
+                    'showgrid': True
+                },
+                'hoverinfo': 'x'
             }
         }
     ]
-    
+
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
-    
+
     # render web page with plotly graphs
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
@@ -78,7 +120,7 @@ def index():
 @app.route('/go')
 def go():
     # save user input in query
-    query = request.args.get('query', '') 
+    query = request.args.get('query', '')
 
     # use model to predict classification for query
     classification_labels = model.predict([query])[0]
